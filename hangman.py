d5afa7683aea0d7394e0ebb6901e7ba31ef33798
Ika Yébenes Herce <ikayherce@gmail.com>
2023-12-18 15:11:17 +0000

    add conditional block for guessing an entire word

--- hangman.py
+++ hangman.py
@@ -27,8 +27,19 @@ def play (word)
             else: 
                 print("Well done", guess, "is in the word." )
                 guessed_letters.append(guess)
+                word_as_list = list(word_completion) 
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess 
+                word_completion = ""join.(word_as_list)
+                if "_" not in word_completion:
+                    guess = True 
         
         elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+                elif guess != word: 
+                    print(guess, "is not the word.")
 
         else: 
             print("Your guess is not valid")
