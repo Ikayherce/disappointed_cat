133d5e63efd869943dcf831637ea7870864261a4
Ika Yébenes Herce <ikayherce@gmail.com>
2023-12-18 13:48:02 +0000

    create function with variables for word completion with number of underscores depending on the length of the word and variables for guessed letters, guessed words and number of tries

--- hangman.py
+++ hangman.py
@@ -4,3 +4,12 @@ from words import word_list
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
+def play (word)
+    word_completion = "_" * len (word)
+    guess = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    
+
