f6d7224d7f0b793c8d31929c23cf486efdbfc5aa
Ika Yébenes Herce <ikayherce@gmail.com>
2023-12-21 19:19:49 +0000

    change print statements to make them more appealing to user

--- run.py
+++ run.py
@@ -3,75 +3,6 @@ from words import word_list
 # show welcome message
 print("Welcome to Sad Kitty. Guess a letter or word right - don't make kitty sad!")
 
-class Level:
-    """
-    Level class :  checks if user wants to play level easy, medium or hard
-    """
-    def __init__(self, level):
-        self.level = level
-
-    def decide_level(self):   
-        if self.level == "1":
-            return "Easy"
-        elif self.level == "2":
-            return "Medium"
-        elif self.level == "3":
-            return "Hard"
-
-
-def validate_level(value):
-    """
-    Checks user gives valid data (1,2 or 3) to choose level
-    """
-    try:
-        if (value != "1") and (value != "2") and (value != "3"):
-            raise ValueError(
-                f"Please only enter 1, 2 or 3. You typed {value}"
-            )
-    except ValueError as e:
-        print(f"Invalid data:{e}, please try again\n")
-        return False
-    return True
-
-"""returns word depending on chosen level """
-def get_level():
-    while True:
-        chosen_level = input(
-            "Choose your level:\n\n 1. Easy\n 2. Medium\n 3. Hard\n")
-        level_instance = Level(chosen_level)  
-        level = level_instance.decide_level()  
-        
-        if validate_level(chosen_level):
-            filtered_list = filter_words(word_list, level)
-            chosen_word = random.choice(filtered_list)
-            return chosen_word
-
-
-
-def filter_words(words, level):
-    """
-    Filters words by length into separate lists depending on chosen level
-    """
-    if level == "Easy":
-        easy = [word for word in words if len(word) < 5]
-        return easy
-    elif level == "Medium":
-        medium = [word for word in words if 5 <= len(word) < 10]
-        return medium
-    elif level == "Hard":
-        hard = [word for word in words if len(word) >= 10]
-        return hard
-
-
-def get_word():
-    """This is the get word function. It gets a random word for the user to guess from the word list in words.py"""
-    word = random.choice(word_list)
-    return word.upper()
-
-
-import random
-from words import word_list
-
 def get_word():
     """  """
     word = random.choice(word_list)
@@ -101,15 +32,15 @@ def play(word):
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 #warns user letter has been already guessed
-                print("You already guessed this letter!", guess)
+                print("You already guessed the letter", guess)
             elif guess not in word:
-                print("Oh no!", guess, "is not in the word.")
+                print("Oh no!", guess, "is not in the word, kitty might cry")
                 #substract tries by 1 when guess is wrong
                 tries -= 1
                 guessed_letters.append(guess)
             else:
                 #print feedback to user when guess is correct
-                print("Well done,", guess, "is in the word.")
+                print("Well done,", guess, "is in the word. Kitty feels good about this")
                 guessed_letters.append(guess)
                 #displays right guesses to user by replacing underscore by correctly guessed letter
                 word_as_list = list(word_completion)
@@ -126,7 +57,7 @@ def play(word):
             if guess in guessed_words:
                 print("You already guessed the word", guess)
             elif guess != word:
-                print(guess, "is not the word.")
+                print(guess, "is not the word. Come on, you can do it!")
                 tries -= 1
                 guessed_words.append(guess)
             else:
@@ -144,10 +75,10 @@ def play(word):
     
     #check if user guessed or ran out of tries,print right feedback to user that they have won or lost the game
     if guessed:
-        print("Congratulations, you guessed the word! You can be proud!")
+        print("Congratulations, you guessed the word! Kitty is proud of you!")
     else:
-        print("Oh no! You ran out of tries.")
-        print(f"The word was {word}. Better luck next time!")
+        print("Oh no! You ran out of tries. Kitty is now sad")
+        print(f"The word was {word}. Kitty wishes you better luck next time!")
 
 
 def display_sad_kitty(tries):
