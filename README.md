3e902760488eb72bcc48455f466f064a35f20e9e
Ika Yébenes Herce <ikayherce@gmail.com>
2023-12-21 14:59:25 +0000

    try to debug by converting print statements to uppercase and input from user to lowercase

--- README.md
+++ README.md
@@ -1,11 +1,90 @@
 
+# Sad Kitty
+
+Sad Kitty is a classic hangman game but the outcome of losing is not seeing a stickfigure hang, it's instead seeing a cat that is very disappointed in you. 
+Sad Kitty is my porftolio project 3 for Code Insitute, coded with Python. 
+
+[Link to live site] 
+
+![amIresponsive image](assets/images/readmeimages/amiresponsive.png)
+
+
+## Index - Table of Contents
+
+- [Planning](#planning)
+
+- [UX](#ux)
+    - [Programm Goals](#programm-goals)
+    - [User Stories](#user-stories)
+
+- [Features](#features)
+    - [Existing Features](#existing-features)
+    - [Future Features](#possible-future-features)
+
+- [Data Model](#data-model)
+
+- [Testing](#testing)
+    - [Validator Testing](#validator-testing)
+    - [Browser Testing](#browser-testing)
+    - [Testing User Stories](#testing-user-stories-functionality)
+
+- [Debugging](#debugging)
+    - [Fixed bugs](#fixed-bugs)
+    - [Unfixed bugs](#unfixed-bugs)
+
+- [Deployment](#deployment)
+
+- [Credits](#credits)
+    - [Data](#data)
+    - [Code](#code)
+    - [Styling](#styling)
+
+## Planning
+
+I planned a simple hangman application where the main parts of the code are the play function, the display function and the logic that allows the user to choose a level of difficulty (easy, medium or hard) and filters the words in the word list (in a separate file) by level. 
+
+## UX 
+
+### Program goals 
+
+The goal of this program is to entertain the user with a simple hangman console game, and at the same time amuse with the twist of a sad cat instead of the classic stick figure. By being able to choose a level of difficulty, the user can play the game several times with variation. 
+
+### User stories 
+
+#### The user wants to
+- see the introduction text
+- see brief and clear instructions
+- choose a level of difficulty (easy, medium or hard)
+- get feedback on correct and incorrect guesses
+- get feedback on when given data is invalid
+- get feedback and see the correct word displayed after losing a game
+- get feedback when game is won and see word displayed
+- get the option to choose whether they want to play again or not 
+
+
+### Features
+#### Existing features
+
+#### Possible future features
+
+### Data Model
+
+### Testing
 
 ## Credits
-Tutorials followed: 
+### Code ### 
+Project on Github by fellow Code Institute student:
+https://github.com/Kathrin-ddggxh/CI-PP3_hangman 
+
+Youtube Tutorials: 
 https://www.youtube.com/watch?v=JNXmCOumNw0
 https://www.youtube.com/watch?v=TWLD2OKmSCQ
 https://www.youtube.com/watch?v=cJJTnI22IF8
 https://www.youtube.com/watch?v=m4nEnsavl6w 
 https://www.youtube.com/watch?v=pFvSb7cb_Us
 
-Class Inspired by project "get hung" of previous project at code institute https://github.com/Kathrin-ddggxh/CI-PP3_hangman 
+### README file ### 
+ https://github.com/Kathrin-ddggxh/CI-PP3_hangman 
+
+### List of words (content) ###
+Chat GPT
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ print("Welcome to Sad Kitty. Guess a letter or word right - don't make kitty sad
 
 class Level:
     """
-    Level class
+    Level class :  checks if user wants to play level easy, medium or hard
     """
     def __init__(self, level):
         self.level = level
@@ -20,7 +20,7 @@ class Level:
 
 def validate_level(value):
     """
-    Checks if user wants to play level easy, medium or hard
+    Checks user gives valid data (1,2 or 3) to choose level
     """
     try:
         if (value != "1") and (value != "2") and (value != "3"):
@@ -32,7 +32,7 @@ def validate_level(value):
         return False
     return True
 
-
+"""returns chosen word depending on chosen level """
 def get_level():
     while True:
         chosen_level = input(
@@ -53,16 +53,12 @@ def filter_words(words, level):
     """
     if level == "Easy":
         easy = [word for word in words if len(word) < 5]
-        #print(f"Easy words: {easy}")   
-        #print(f"Filtered list for Easy: {easy}") 
         return easy
     elif level == "Medium":
         medium = [word for word in words if 5 <= len(word) < 10]
-        #print(f"Medium words: {medium}")   
         return medium
     elif level == "Hard":
         hard = [word for word in words if len(word) >= 10]
-        #print(f"Hard words: {hard}")  # Add this line
         return hard
 
 
@@ -80,67 +76,62 @@ def play(word):
     guessed_letters = [] #stores guessed letters
     guessed_words = [] #stores guessed words
     tries = 6 #number of tries before the whole sad kitty is on full display
-    print(display_hangman(tries))
+    print(display_sad_kitty(tries))
     print(word_completion)
     print("\n")
 
     word_letters = list(word)
 
     while not guessed and tries > 0:
-        guess = input("Please guess a letter, or the whole word:\n ").upper()
-        print(f"DEBUG: The word is {''.join(word_letters)}")  # Debugging print to see the word
+        #guess = input("Please guess a letter, or the whole word:\n ").upper()
+        guess = input("Please guess a letter, or the whole word:\n ").lower()
 
         # check that user data is one letter or a word of the riht length
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters: #warns user letter has been already guessed
-                print("You already guessed this letter!", guess)
-            elif guess not in word:
-                print(f"DEBUG: The word is {word}")  # Debugging print
-                print("Oh no!", guess, "is not in the word.")  
-                print(f"DEBUG: Result of comparison: {guess in word_letters}")  # Debugging print
+                print("You already guessed this letter!", guess.upper)
+            #elif guess not in word:
+            elif guess.lower() not in word: 
+                print("Oh no!", guess, "is not in the word.")
                 tries -= 1  #substract tries by 1 when guess is wrong
                 guessed_letters.append(guess)
       
 
             else:
-                print(f"DEBUG: The guessed letter {guess} is in the word.")  # Debugging print
-                #print("Well done,", guess, "is in the word.")#print feedback to user when guess is correct
-                print(f"DEBUG: Result of comparison: {guess in word_letters}")  # Debugging print
-                print(f"DEBUG: Result of comparison: {guess in word}")  # Debugging print
-                print("Indices of", guess, "in word:", [i for i, letter in enumerate(word) if letter == guess]) # Debugging print
+                print("Well done,", guess, "is in the word.")#print feedback to user when guess is correct
                 guessed_letters.append(guess)
                 #displays right guesses to user by replacing underscore by correctly guessed letter
                 word_as_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
-                word_completion = "".join(word_as_list)
-                #if all underscores are replaced by correctly guessed letters, word is guessed
-                print("Current word_completion:", word_completion)  # Debugging print
+                word_completion = "".join(word_as_list)#if all underscores are replaced by  letters, word is guessed
 
-                if "_" not in word_completion:
-                    guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
-            #conditional block checks if word is guessed, if letter is already guessed 
-            #and whether letter is or is not in the word. 
-            if guess in guessed_words:
-                print("You already guessed the word", guess)
+        
+
+            '''conditional block checks if word is guessed, if letter is already guessed 
+            and whether letter is or is not in the word. '''
+            if "_" not in word_completion:
+                guessed = True
+            elif len(guess) == len(word) and guess.isalpha():
+                if guess in guessed_words:
+                    print("You already guessed the word", guess.upper)
             elif guess != word:
-                print(guess, "is not the word.")
+                print(guess.upper, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completion = word
+                word_completion = word  
         else:
             #if user has not provided a letter or word of correct length they get feedback that guess is invalid
             word_length = len(word)
             print("Your guess is not valid.")
             print(f"It needs to be a letter or a word of {word_length} length")
-        #print display of hangman and of word completion
-        print(display_hangman(tries))
-        print(word_completion)
-        print("\n")
+            #print display of sad kitty and of word completion
+            print(display_sad_kitty(tries))
+            print(word_completion)  
+            print("\n")
     
     #check if user guessed or ran out of tries,print right feedback to user that they have won or lost the game
     if guessed:
@@ -150,7 +141,7 @@ def play(word):
         print(f"The word was {word}. Kitty wishes you better luck next time!")
 
 
-def display_hangman(tries):
+def display_sad_kitty(tries):
     """
     This is the display function. 
     It displays the different stages of display of the disappointed cat, one stage for each failed try. 
@@ -231,11 +222,11 @@ def main():
     """
     This function runs the game and asks the user if they want to play again when the game is over.
     """
-    word = get_level()  # Changed this line
+    word = get_level()  
     play(word)
     
-    while input("Do you want to play again? (Y/N): ").upper() == "Y":
-        word = get_level()  # And this line
+    while input("Do you want to play again? (Y/N): ")() == "Y":
+        word = get_level()  
         play(word)
 
 #code fragment so game runs by running the script on the command line
